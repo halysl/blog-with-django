19adf13a9037eadca35a38c26e69c7f3e1dda545
yagami light <halysl0817@gmail.com>
2019-09-19 10:04:24 +0800

    替换 post

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,5 @@ from .models import Post
 def index(request):
     post_list = Post.objects.all().order_by("-created_time")
     return render(request, 'blog/index.html', context={
-        'post_list': f'post_list'
+        'post_list': post_list
     })
