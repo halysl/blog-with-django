1ef2339cb46fa522578d87b7b79f4a84a6ad749d
yagami light <halysl0817@gmail.com>
2019-09-29 17:13:25 +0800

    add 后台创作

--- blog/models.py
+++ blog/models.py
@@ -4,26 +4,37 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = "分类"
+        verbose_name_plural = verbose_name
+
+    name = models.CharField("分类名", max_length=100)
 
     def __str__(self):
         return self.name
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = "标签"
+        verbose_name_plural = verbose_name
+    name = models.CharField("标签名", max_length=100)
 
     def __str__(self):
         return self.name
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=70)
-    body = models.TextField()
-    excerpt = models.CharField(max_length=200, blank=True)
+    class Meta:
+        verbose_name = "文章"
+        verbose_name_plural = verbose_name
 
-    created_time = models.DateTimeField()
-    modified_time = models.DateTimeField()
+    title = models.CharField("标题", max_length=70)
+    body = models.TextField("文章内容", )
+    excerpt = models.CharField("文章摘要", max_length=200, blank=True)
+
+    created_time = models.DateTimeField("创建时间", )
+    modified_time = models.DateTimeField("修改时间", )
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
