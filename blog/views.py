e9287cf92f085fd5e08196961c0024e28ef8fd34
yagami light <halysl0817@gmail.com>
2019-10-01 18:52:44 +0800

    add 完成分类、归档和标签页功能

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.utils.text import slugify
 from markdown.extensions.toc import TocExtension
 
 # Create your views here.
-from .models import Post
+from .models import Post, Category, Tag
 
 
 def index(request):
@@ -28,3 +28,22 @@ def detail(request, pk):
     m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md_obj.toc, re.S)
     post.toc = m.group(1) if m is not None else ''
     return render(request, "blog/detail.html", context={"post": post})
+
+
+def archive(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={"post_list": post_list})
+
+
+def tag(request, pk):
+    t = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tags=t).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
